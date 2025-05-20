82cd4f6d0cd9c588be6df6f0870f03a63303f653
Wesley Leite <leitewesleyleite@gmail.com>
2025-05-20 16:03:35 -0300

    Ajuste da Program.cs

--- Contato.Atualizar.Web/Program.cs
+++ Contato.Atualizar.Web/Program.cs
@@ -2,8 +2,10 @@ using System.Reflection;
 using Contato.Atualizar.Application.Interfaces;
 using Contato.Atualizar.Application.Services;
 using Contato.Atualizar.Infra.RabbitMQ;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
+Console.WriteLine($"Ambiente atual: {builder.Environment.EnvironmentName}");
 
 // Add services to the container.
 
@@ -12,10 +14,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IContatoService, ContatoService>();
-
 builder.Services.Configure<RabbitMQSettings>(builder.Configuration.GetSection("RabbitMQ"));
 builder.Services.AddSingleton<IRabbitMqProducer, RabbitMqProducer>();
+
+builder.Services.AddScoped<IContatoService, ContatoService>();
 
 builder.Services.AddSwaggerGen(options =>
 {
@@ -24,21 +26,25 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(xmlPath);
 });
 
+// Define a porta HTTP
+builder.WebHost.UseUrls("http://*:8080");
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
-}
+// (Opcional) Habilitar CORS para liberar requisições de qualquer origem
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Middleware Prometheus para requisições HTTP — deve vir antes da autorização
+app.UseHttpMetrics();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Mapear /metrics depois dos controllers para evitar conflitos de rota
+app.MapMetrics();
 
 app.Run();
